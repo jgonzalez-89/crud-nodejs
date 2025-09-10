0408c86505ad5015c434d9bb48ca1b602c96084a
jgonzalez-89 <joseluis@gonzalezdiaz.dev>
2025-09-10 11:54:08 +0200

    feat(ProductRepository): port interface

---
+++ src/domain/ports/ProductRepository.ts
@@ -0,0 +1,9 @@
+import { Product } from '../models/Product';
+
+export interface ProductRepository {
+  save(product: Product): Promise<Product>;
+  findById(id: number): Promise<Product | null>;
+  findAll(): Promise<Product[]>;
+  update(id: number, product: Product): Promise<Product | null>;
+  delete(id: number): Promise<boolean>;
+}
